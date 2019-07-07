2b164facd5de528d1190a284866dd5570149e0dc
benneee <benedictiknkeonye@gmail.com>
2019-07-07 08:19:15 +0100

    Set up components and sorted API connections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import bootstrap from "bootstrap";
+import { PokemonCardComponent } from "./pokemon-card/pokemon-card.component";
+import { PokemonGroupComponent } from "./pokemon-group/pokemon-group.component";
+import { PokemonViewComponent } from "./pokemon-view/pokemon-view.component";
+import { PokemonComponent } from "./pokemon/pokemon.component";
+import { PokemonService } from "./providers/pokemon.service";
+import { NavComponent } from "./nav/nav.component";
+import { HttpClientModule } from "@angular/common/http";
+import { CapitalizePipe } from './capitalize.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonCardComponent,
+    PokemonGroupComponent,
+    PokemonViewComponent,
+    PokemonComponent,
+    NavComponent,
+    CapitalizePipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pokemon-group/pokemon-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-group',
+  templateUrl: './pokemon-group.component.html',
+  styleUrls: ['./pokemon-group.component.css']
+})
+export class PokemonGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
